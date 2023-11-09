7e75ee25125b23712a92f9af1be91f69128d5f4b
DBiukRosic <dbiukr00@fesb.hr>
2023-11-09 02:38:19 +0100

    Added posts to Find Our Shops page

--- semester-project/app/findourshops/page.tsx
+++ semester-project/app/findourshops/page.tsx
@@ -1,9 +1,35 @@
-function FindourShops(){
-    return(
-    <main className='flex justify-center text-4x1 p-14'>
-        <h1>Find our shops</h1>
-      </main>
-    )
+import Link from "next/link";
+
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
-export default FindourShops;
+const BASE_API_URL = "https://jsonplaceholder.typicode.com";
+
+const getPosts = async (): Promise<Post[]> => {
+  const data = await fetch(`${BASE_API_URL}/posts`);
+  return data.json();
+};
+
+export default async function FindOurShops() {
+  const posts = await getPosts();
+  return (
+    <main className="flex flex-col items-center min-h-screen max-w-5xl m-auto p-10">
+      <h1 className="text-3xl font-bold p-10">Locations of our shops</h1>
+      <ul className="flex flex-col gap-8">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`findourshops/${post.id}`}>
+              <span className="text-2xl text-purple-500">
+                Post {post.title}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
